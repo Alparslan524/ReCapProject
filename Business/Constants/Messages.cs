39a25fc6d8abe1f054498fbd0b20e7947f0d00d4
Alparslan <alparslanaydgn02@gmail.com>
2022-12-01 13:33:06 +0300

    Result Yapısı eklendi

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string CarAdded = "Araba Eklendi";
+        public static string CarDeleted = "Araba Silindi";
+        public static string CarUpdated = "Araba Güncellendi";
+        public static string CarListed = "Araba Listelendi";
+        public static string ListedByPrice = "Fiyat Aralığına Göre Listelendi";
+        public static string CarDetailListed = "Ürün Detayları Listelendi";
+        public static string FalseDailyPrice = "Günlük Fiyat Sıfırdan Büyük Olmalıdır";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        public Result(bool success, string message) : this(success)
+        {
+            //read-only olmasına rağmen constructorda sett edilebilir!!!!
+            this.Message = message;
+        }
+        public Result(bool success)
+        {
+            //Consturctor overloading
+            this.Success = success;
+        }
+    }
+}
